8cd18e7186bde320cc0f41a65ad7383483b01ece
syedubaid <syedabuubaid@gmail.com>
2024-05-17 19:40:48 +0530

    commit

--- memoryMemory/src/App.jsx
+++ memoryMemory/src/App.jsx
@@ -2,27 +2,21 @@ import { useState } from "react";
 import { useEffect } from "react";
 import Navbar from "../components/nav";
 import Cards from "../components/cards";
+import axios from "axios";
 
 export default function App(){
-  useEffect(()=>{
 
-  
-  const fetchImages=async ()=>{
-    try{
-    const response= await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    if(!response){
-      throw new Error("Cannot fetch data")
-    }
+  const[url,setUrl]=useState("https://pokeapi.co/api/v2/pokemon/")
 
-    const data=await response.json();
-    console.log(data);
-    }
-    catch (error) {
-      setError(error.message);
-    }
+  const getPoke=async()=>{
+    const res=await axios.get(url)
+    console.log(res.data.results.name)
   }
-  
-},[])
+
+  useEffect(()=>{
+  getPoke();
+},[url])
+
   return(
     <>
     <Navbar/>
