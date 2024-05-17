5953f4cdbc8530b420b902421463d8de5365e62a
syedubaid <syedabuubaid@gmail.com>
2024-05-17 17:00:32 +0530

    api fetch check

--- memoryMemory/components/cards.jsx
+++ memoryMemory/components/cards.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Cards(props){
+    
 
 
 
--- memoryMemory/src/App.jsx
+++ memoryMemory/src/App.jsx
@@ -1,8 +1,28 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import Navbar from "../components/nav";
 import Cards from "../components/cards";
 
 export default function App(){
+  useEffect(()=>{
+
+  
+  const fetchImages=async ()=>{
+    try{
+    const response= await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    if(!response){
+      throw new Error("Cannot fetch data")
+    }
+
+    const data=await response.json();
+    console.log(data);
+    }
+    catch (error) {
+      setError(error.message);
+    }
+  }
+  
+},[])
   return(
     <>
     <Navbar/>
